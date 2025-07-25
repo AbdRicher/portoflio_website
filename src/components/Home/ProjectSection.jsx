7202f41df39e85fa817619c9c853af9b316a861f
AbdRicher <f2022266614@umt.edu.pk>
2025-07-25 17:45:48 +0500

    Bug fixes

--- src/components/Home/ProjectSection.jsx
+++ src/components/Home/ProjectSection.jsx
@@ -5,7 +5,7 @@ export default function ProjectSection() {
   return (
     <div className='mt10 flex flex-col justify-center items-center'>
         <h1 className=' my-10 text-4xl font-bold leading-tight' >Recent Projects</h1>
-      <CarouselDarkVariantExample title={"American Roofing Next.js Project"} path={["/am_roof/slide1.png","/am_roof/slide2.png","/am_roof/slide3.png"]} label={["Front Page","FAQs Section","Footer"]}/>
+      <CarouselDarkVariantExample title={"American Roofing Next.js Project"} path={["/am_roof/slide1.png","/am_roof/slide2.png","/am_roof/slide3.png"]} label={["Interface","Form Submission","Contact (More on Work Page)"]}/>
     </div>
   )
 }
